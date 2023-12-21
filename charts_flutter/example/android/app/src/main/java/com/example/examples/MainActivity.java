b21a04c7999033868acf1cf9caa2b2daecd28234
cuong.truong <cuong.truong@ecotruck.vn>
2023-12-21 11:06:23 +0700

    up code

--- charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
+++ charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.examples;
 
-import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 /**
  * FlutterActivity
  */
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Your existing code
+                        }
+                );
+    }
 }
